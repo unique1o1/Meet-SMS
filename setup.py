1496c8cef040e435c4aaae9ccdf1e2444f9f6d9e
yunik maharjan <yunik.maharjan@icloud.com>
2018-01-10 22:29:40 +0545

    fix typo

--- setup.py
+++ setup.py
@@ -3,21 +3,22 @@ import setuptools
 setuptools.setup(
     name="MeetSMS",
     version="1.1",
-    url="https://github.com/unique1o1/SimpleSMS",
+    url="https://github.com/unique1o1/Meet-SMS",
 
     author="Yunik Maharjan",
     author_email="yunik.maharjan@icloud.com",
-
+    license='MIT',
     description="Send SMS easily from MeetSMS",
     long_description=open('README.md').read(),
 
     packages=setuptools.find_packages(),
 
     install_requires=['requests'],
-
+    platform=['unix'],
     classifiers=[
         'Environment :: Console',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
     ],
     entry_points="""
     [console_scripts]
